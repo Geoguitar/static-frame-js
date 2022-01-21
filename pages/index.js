2c5ecd83eff8c3079e11c6db971a9244f0166e1e
Geoguitar <geovanedsilveira@gmail.com>
2022-01-20 21:54:48 -0300

    Criado primeira pagina com JavaScript

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Link from "next/link"
+
+import {useState} from 'react';
+
+function Home() {
+    return (<div>
+        <h1>Home</h1>
+        <Contador/>
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>) 
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>                      
+    )       
+    
+}
+
+export default Home
+
+
+
+
+
